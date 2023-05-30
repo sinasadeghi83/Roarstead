c3ef1a8242edce31c2c62557d3a3151623928dbc
Amirhossein <Amirmemool12@gmail.com>
2023-05-30 18:52:53 -0400

    :hammer:chore:implement the DBClass

--- src/main/java/com/DateBase/DBClass.java
+++ src/main/java/com/DateBase/DBClass.java
@@ -1,4 +1,24 @@
 package com.DateBase;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class DBClass {
+    private Session session;
+
+    public DBClass() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        setSession(sessionFactory.openSession());
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
 }
