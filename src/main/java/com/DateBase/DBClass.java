a1183bf4bac9461576374c2d418d8f294120f3d4
Amirhossein <Amirmemool12@gmail.com>
2023-05-30 19:10:22 -0400

    :hammer:chore:add more method to DateBase class

--- src/main/java/com/DateBase/DBClass.java
+++ src/main/java/com/DateBase/DBClass.java
@@ -6,19 +6,31 @@ import org.hibernate.cfg.Configuration;
 
 public class DBClass {
     private Session session;
+    private boolean sessionIsOpen;
+    private Configuration configuration;
+    private SessionFactory sessionFactory;
 
     public DBClass() {
-        Configuration configuration = new Configuration();
+        configuration = new Configuration();
         configuration.configure();
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        setSession(sessionFactory.openSession());
+        sessionFactory = configuration.buildSessionFactory();
+        sessionIsOpen = false;
     }
 
     public Session getSession() {
         return session;
     }
-
     public void setSession(Session session) {
         this.session = session;
+    }
+    
+    public void openSession() {
+        setSession(sessionFactory.openSession());
+        sessionIsOpen = true;
+    }
+
+    public void closeSession() {
+        session.close();
+        sessionIsOpen = false;
     }
 }
