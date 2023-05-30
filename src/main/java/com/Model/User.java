58b7b6933bc5926d637f8d7b488fb7a11e3f58a8
Amirhossein <Amirmemool12@gmail.com>
2023-05-30 18:58:53 -0400

    :hammer:chore:implement User class

--- src/main/java/com/Model/User.java
+++ src/main/java/com/Model/User.java
@@ -1,4 +1,37 @@
 package com.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "test",schema = "test_schema")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String userName;
+
+    public User() {
+
+    }
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
