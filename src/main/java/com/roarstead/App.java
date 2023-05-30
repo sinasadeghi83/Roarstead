c8e803a00ce605d12fa5cf33090bdf20f7ac4b9b
Amirhossein <Amirmemool12@gmail.com>
2023-05-30 19:16:25 -0400

    :white_check_mark:test:test the hibernate connection to postgresql

--- src/main/java/com/roarstead/App.java
+++ src/main/java/com/roarstead/App.java
@@ -1,13 +1,22 @@
 package com.roarstead;
 
+import com.DateBase.DBClass;
+import com.Model.User;
+import org.hibernate.Transaction;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        DBClass db = new DBClass();
+        db.openSession();
+        User user = new User("amirhossein");
+        Transaction transaction = db.getSession().getTransaction();
+        transaction.begin();
+        db.getSession().save(user);
+        transaction.commit();
+        db.closeSession();
     }
 }
