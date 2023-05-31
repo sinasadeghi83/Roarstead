91282e86f8515453c6d8792854736f0367397d06
Amirhossein <Amirmemool12@gmail.com>
2023-05-31 10:39:56 -0400

    :hammer: chore : change the User class

--- src/main/java/com/roarstead/Model/User.java
+++ src/main/java/com/roarstead/Model/User.java
@@ -3,20 +3,38 @@ package com.roarstead.Model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "test",schema = "test_schema")
+@Table(name = "test", schema = "test_schema")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @Column
-    private String userName;
+    private String userName;  //unique
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;     //unique and formatting
+    @Column
+    private String phone;     //unique and formatting
+    @Column
+    private String password;        //should get it twice
+    @Column
+    private String date;            //need formatting
 
     public User() {
 
     }
 
-    public User(String userName) {
+    public User(String userName, String firstName, String lastName, String email, String phone, String password, String date) {
         this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.date = date;
     }
 
     public int getId() {
@@ -33,5 +51,53 @@ public class User {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
