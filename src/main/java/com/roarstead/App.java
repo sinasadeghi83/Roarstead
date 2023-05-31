68fa8e6a90b029d1db0a13525cd8647b990714e6
Amirhossein <Amirmemool12@gmail.com>
2023-05-31 10:43:58 -0400

    :package: chore : add country file

--- src/main/java/com/roarstead/App.java
+++ src/main/java/com/roarstead/App.java
@@ -10,13 +10,13 @@ import org.hibernate.Transaction;
  */
 public class App {
     public static void main( String[] args ) {
-        DBClass db = new DBClass();
-        db.openSession();
-        User user = new User("amirhossein");
-        Transaction transaction = db.getSession().getTransaction();
-        transaction.begin();
-        db.getSession().save(user);
-        transaction.commit();
-        db.closeSession();
+//        DBClass db = new DBClass();
+//        db.openSession();
+//        User user = new User("amirhossein");
+//        Transaction transaction = db.getSession().getTransaction();
+//        transaction.begin();
+//        db.getSession().save(user);
+//        transaction.commit();
+//        db.closeSession();
     }
 }
