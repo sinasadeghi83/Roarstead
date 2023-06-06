262918c9698e9ab2778baf9a8cb652157d17621b
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-06-06 12:36:21 +0330

    :hammer: chore: Add Unproccesable Entity Error

--- src/main/java/com/roarstead/Components/Response/Response.java
+++ src/main/java/com/roarstead/Components/Response/Response.java
@@ -3,17 +3,20 @@ package com.roarstead.Components.Response;
 public class Response {
     public static final String NOT_FOUND_MSG = "404 Not found";
     public static final String INTERNAL_ERROR_MSG = "500 Server internal error. Contact the server admin.";
+    public static final String UNPROCESSABLE_ENTITY_MSG = "422 Unprocessable Entity";
     public static final int NOT_FOUND = 404;
     public static final int INTERNAL_ERROR = 500;
-  
-    private String message;
+    public static final int OK = 200;
+    public static final int UNPROCESSABLE_ENTITY = 422;
+
+    private Object message;
     private int code;
 
-    public Response(String message, int code) {
+    public Response(Object message, int code) {
         this.message = message;
         this.code = code;
     }
-    public String getMessage() {
+    public Object getMessage() {
         return message;
     }
 
