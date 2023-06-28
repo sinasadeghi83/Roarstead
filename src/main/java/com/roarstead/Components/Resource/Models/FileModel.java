304baae8a2e207cc06b7f00fc3be11f32af2813a
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-06-28 17:07:40 +0330

    :hammer: Add FileModel

---
+++ src/main/java/com/roarstead/Components/Resource/Models/FileModel.java
@@ -0,0 +1,55 @@
+package com.roarstead.Components.Resource.Models;
+
+import com.roarstead.App;
+import com.roarstead.Components.Database.Database;
+import com.roarstead.Components.Resource.Converters.FileConverter;
+import jakarta.persistence.*;
+
+import java.io.File;
+
+@Entity
+@Table(name = "files")
+public class FileModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @Column(name = "file_path")
+    @Convert(converter = FileConverter.class)
+    private File file;
+
+    public FileModel(){
+    }
+
+    public FileModel(String fileName, File file) {
+        this.fileName = fileName;
+        this.file = file;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+}
