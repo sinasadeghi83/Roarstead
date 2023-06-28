6184f282a01965ebfeb96a5c0b7e19be5f508721
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-06-28 17:20:33 +0330

    :hammer: Change Profile to store createdAt and images as Image

--- src/main/java/com/roarstead/Models/Profile.java
+++ src/main/java/com/roarstead/Models/Profile.java
@@ -1,10 +1,17 @@
 package com.roarstead.Models;
 
+import com.roarstead.App;
+import com.roarstead.Components.Database.Database;
 import com.roarstead.Components.Resource.Models.Image;
+import com.roarstead.Components.Resource.ResourceManager;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.checkerframework.checker.units.qual.C;
+
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 @Embeddable
 public class Profile {
@@ -17,19 +24,22 @@ public class Profile {
     @JoinColumn(name="header_img_id", referencedColumnName = "id")
     private Image headerImage;
 
+    @Column(name = "bio")
     @Size(min = 0,max = 160,message = "bio must be between 0 and 160 characters")
     private String bio;
 
+    @Column
     @NotNull(message = "location can not be NULL")
     private String location;
 
     @Column(name = "url")
     private URL webSiteLink;
 
-    @NotNull(message = "joinDate con not be NUll")
-    private Date joinDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", insertable = true, updatable = false)
+    private Date createdAt;
 
-    public Object getProfImage() {
+    public Image getProfImage() {
         return profImage;
     }
 
@@ -37,7 +47,7 @@ public class Profile {
         this.profImage = profImage;
     }
 
-    public Object getHeaderImage() {
+    public Image getHeaderImage() {
         return headerImage;
     }
 
@@ -69,12 +79,25 @@ public class Profile {
         this.webSiteLink = webSiteLink;
     }
 
-    public Date getJoinDate() {
-        return joinDate;
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
-    public void setJoinDate(Date joinDate) {
-        this.joinDate = joinDate;
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void deleteProfileImage(User user) {
+        if(this.profImage == null)
+            return;
+        Database db = App.getCurrentApp().getDb();
+        Image profImageTemp = this.profImage;
+        db.ready();
+        this.profImage = null;
+        db.getSession().update(user);
+        db.done();
+        ResourceManager resourceManager = App.getCurrentApp().getResourceManager();
+        resourceManager.deleteImage(profImageTemp);
     }
 }
 
