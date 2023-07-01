c0df023a7320d2ca65de9a7fd229901f54a09279
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-07-01 21:31:03 +0330

    :sparkles: :ambulance: Add profile/image-header and Fix none images

--- src/main/java/com/roarstead/Controllers/ProfileController.java
+++ src/main/java/com/roarstead/Controllers/ProfileController.java
@@ -18,8 +18,32 @@ public class ProfileController extends BaseController {
     @Override
     public Map<String, List<String>> accessControl() {
         return Map.of(
-            "actionAvatar", List.of("@")
+                "actionAvatar", List.of("@"),
+                "actionImageHeader", List.of("@")
         );
+    }
+
+    public Response actionImageHeader() throws Exception{
+        Map<String, String> params = App.getCurrentApp().getQueryParams();
+        int userId = Integer.parseInt(params.get(USER_ID_KEY));
+
+        Database db = App.getCurrentApp().getDb();
+        User user = (User) App.getCurrentApp().getAuthManager().identity();
+
+        User wantedUser = db.getSession()
+                .createQuery("FROM User WHERE id=:userId", User.class)
+                .setParameter("userId", userId)
+                .getSingleResultOrNull();
+        if(user == null)
+            throw new NotFoundException();
+
+        if(user.getBlockedByList().contains(wantedUser)){
+            throw new ForbiddenException(User.FORBIDDEN_BLOCKED_ERR);
+        }
+
+        Image imageHeader = wantedUser.getProfile().getHeaderImage();
+        String extension = imageHeader != null ? imageHeader.getExtension() : "png";
+        return new Response(App.getCurrentApp().getResourceManager().convertImageToBase64(imageHeader), Response.OK, "image/" + extension);
     }
 
     public Response actionAvatar() throws Exception{
@@ -41,6 +65,7 @@ public class ProfileController extends BaseController {
         }
 
         Image avatar = wantedUser.getProfile().getProfImage();
-        return new Response(App.getCurrentApp().getResourceManager().convertImageToBase64(avatar), Response.OK, "image/" + avatar.getExtension());
+        String extension = avatar != null ? avatar.getExtension() : "png";
+        return new Response(App.getCurrentApp().getResourceManager().convertImageToBase64(avatar), Response.OK, "image/" + extension);
     }
 }
