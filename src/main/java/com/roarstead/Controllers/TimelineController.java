c6a0272419ade2846875c40f58a6a87465043567
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-07-01 19:29:26 +0330

    :sparkles: Add timeline/hashtag

--- src/main/java/com/roarstead/Controllers/TimelineController.java
+++ src/main/java/com/roarstead/Controllers/TimelineController.java
@@ -1,9 +1,12 @@
 package com.roarstead.Controllers;
 
+import com.google.gson.JsonObject;
 import com.roarstead.App;
 import com.roarstead.Components.Controller.BaseController;
 import com.roarstead.Components.Database.Database;
+import com.roarstead.Components.Exceptions.BadRequestException;
 import com.roarstead.Components.Exceptions.NotAuthenticatedException;
+import com.roarstead.Components.Exceptions.NotFoundException;
 import com.roarstead.Components.Response.Response;
 import com.roarstead.Models.GRoar;
 import com.roarstead.Models.Roar;
@@ -14,13 +17,31 @@ import java.util.Map;
 
 public class TimelineController extends BaseController {
     private static final int FAV_STAR_LIMIT = 10;
+    private static final String HASHTAG_NAME_KEY = "hashtag_name";
 
     @Override
     public Map<String, List<String>> accessControl() {
         return Map.of(
-            "actionFollowing", List.of("@"),
-                "actionForYou", List.of("@")
+                "actionFollowing", List.of("@"),
+                "actionForYou", List.of("@"),
+                "actionHashtag", List.of("@")
         );
+    }
+
+    public Response actionHashtag() throws Exception{
+        Map<String, String> params = App.getCurrentApp().getQueryParams();
+        String hashtagName = params.get(HASHTAG_NAME_KEY);
+        if(hashtagName == null)
+            throw new BadRequestException();
+
+        Database db = App.getCurrentApp().getDb();
+
+        List<Roar> roars = db.getSession()
+                .createQuery("FROM Roar r JOIN r.hashtags h WHERE h.name=:hashtagName", Roar.class)
+                .setParameter("hashtagName", hashtagName)
+                .getResultList();
+
+        return new Response(roars, Response.OK);
     }
 
     public Response actionForYou() throws Exception {
